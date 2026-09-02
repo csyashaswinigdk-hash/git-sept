8cc814446aeefe96328cc2e8dd43bde81855a692
csyashaswinigdk-hash <csyashaswinigdk@gmail.com>
2026-09-02 22:49:04 +0530

    deveops.py

---
+++ devops.py
@@ -0,0 +1,10 @@
+print("git")
+print("github")
+print("jenkins")
+print("docker")
+print("kubernetes")
+print("maven")
+print("ansible")
+print("terraform")
+print("aws")
+print("sonarqube")git init
